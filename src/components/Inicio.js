2008e9f83094c8bc15b272c3db6e9b71b209e5bd
Kevin S <144287002+Curitagames@users.noreply.github.com>
2024-07-09 19:40:28 -0500

    Update Inicio.js
    Se actualizó el párrafo de la portada

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -29,7 +29,7 @@ function Inicio() {
                     <h1 className='titulo-inicio' data-aos="fade-up">Excelencia en Servicios Profesionales de Higiene y Saneamiento Ambiental</h1>
                     <p className='parrafo-inicio' data-aos="fade-up">
                         Ofrecemos soluciones integrales de higiene y saneamiento ambiental para garantizar entornos seguros y saludables. <br/>
-                        Para conocer más haga clic en el botón
+                        Para obtener nuestro Brochure, haga clic en el botón
                     </p>
                     <button className='boton-conoce-mas' onClick={openModal} data-aos="fade-up">Conoce más</button>
                     <ModalForm isOpen={modalIsOpen} onRequestClose={closeModal} />
